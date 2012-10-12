7729b1d942cc6513d9d159c1610d2598f17e9abd
Andy T. <contact@vietcoop.com>
2012-10-12 22:19:49 +0700

    We need to store KeyPic token with the entities.

--- keypic.api.php
+++ keypic.api.php
@@ -6,5 +6,5 @@
 /**
  * Implements hook_keypic_validate($response, $form, $form_state)
  */
-function hook_keypic_validate($response, $form, $form_state) {
+function hook_keypic_validate($response, $token, $form, $form_state) {
 }
