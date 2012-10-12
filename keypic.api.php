b4ae1954aa5913f133a860910c9440001ac8bd88
Andy T. <contact@vietcoop.com>
2012-10-12 21:02:47 +0700

    Allow other modules to hook to keypic validate event.

---
+++ keypic.api.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file keypic.api.php
+ */
+
+/**
+ * Implements hook_keypic_validate($response, $form, $form_state)
+ */
+function hook_keypic_validate($response, $form, $form_state) {
+}
